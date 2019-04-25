cf7e4e9cba3fac9fdf8e72ebfacbd7f4b57a0cb1
Mu Zhou <47339917+zhoumu789@users.noreply.github.com>
2019-04-24 21:20:36 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+import reducer from './reducer/counter';
+
+const store = createStore(reducer);
+
+//store.subscribe(() => console.log("State updated!", store.getState()));
+
+
+const render = () => {
+	ReactDOM.render(
+		<App
+			onIncrement={ () => store.dispatch({ type: "INCREMENT" }) }
+			onDecrement={ () => store.dispatch({ type: "DECREMENT" }) }
+			value={ store.getState() }
+		/>, document.getElementById('root'));	
+} 
+
+render();
+
+store.subscribe(render);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
